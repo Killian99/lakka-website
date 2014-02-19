446403d24c4184a8000ec269d3a6087b667a292e
Niouby <alonso.morgane@gmail.com>
2014-02-19 19:14:18 +0100

    Fix email injections. Closes #1

--- target.php
+++ target.php
@@ -33,31 +33,46 @@
       <?php include("includes/menu.php"); ?>
     </header>
 
-<?php if(isset($_POST) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message']))
-{
-	if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['message']))
-	{
-		$to = 'lakka+alonso.morgane@gmail.com';
-		$subject = $_POST['subject'];
-		
-		$message = $_POST['message'];
-		
-		$headers = 'From : '.$_POST['name'].' <'.$_POST['email'].'>';
+<?php
+  function spamcheck($field)
+  {
+    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
+    if (filter_var($field, FILTER_VALIDATE_EMAIL)) 
+    {
+      return 1;
+    }
+  }
 
-		if (mail($to, $subject, $message, $headers))
-		{
-			echo 'Email successfully sent!'; 
-		} 
-		else 
-		{
-		 	echo "Email delivery failed…";
-		}
-	}
-	else
-	{
-		echo "error2";
-	}
-}
+  if(isset($_POST) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message']))
+  {
+    if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['message']))
+    {
+      if (spamcheck($_POST["email"])) 
+      {
+        $to = 'alonso.morgane@gmail.com,jean.andre.santoni@gmail.com';
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+        $headers = 'From : '.$_POST['name'].' <'.$_POST['email'].'>';
+  
+        if (mail($to, $subject, $message, $headers))
+        {
+            echo 'Email successfully sent!'; 
+        } 
+        else 
+        {
+            echo "Email delivery failed…";
+        }
+      }
+      else
+      {
+        echo "Invalid email";
+      }
+    }
+    else
+    {
+      echo "Invalid input";
+    }
+  }
 
 ?>
 
