1ffd27362350ce29eb1a40c834bf6ec3952e077b
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-02-21 01:08:35 +0100

    php menu

---
+++ home.php
@@ -0,0 +1,43 @@
+<header>
+ <aside class="clearer container">
+  <div class="column">
+   <h2>The DIY retro emulation&nbsp;console</h2>
+   <p class="subtitle">Lakka is a lightweight Linux distribution that transforms a credit card sized computer into a full blown emulation console.</p>
+   <a class="getlakka" href="https://github.com/Niouby/Lakka">Get Lakka</a>
+  </div>
+  <div class="tv">
+    <video src="videos/menu.ogv" preload="auto" autoplay="true" loop="true"></video>
+  </div>
+ </aside>
+</header>
+
+<section id="about" class="clearer">
+  <article class="clearer">
+    <i class="fa fa-bolt"></i>
+    <div>
+      <h3>Powerfull</h3>
+      <p>Built on top of the famous RetroArch emulator, Lakka is able to emulate a large range of hardware and has some usefull features such as Braid-like rewinding, joypad hotplug and video streaming.</p>
+    </div>
+  </article>
+  <article class="clearer">
+    <i class="fa fa-smile-o"></i>
+    <div>
+      <h3>User friendly</h3>
+      <p>Lakka is easy to setup and use. Once installed to your SD card, you just have to put your rom on the card, plug your joypad and enjoy your favorite old games.</p>
+    </div>
+  </article>
+  <article class="clearer">
+    <i class="fa fa-thumbs-o-up"></i>
+    <div>
+      <h3>Low cost</h3>
+      <p>The hardware required to run Lakka cost only $35. We also support PS3 and XBox360 controllers so you don't have to buy new ones. The power can be supplied by any micro USB adapter like the one of your smartphone.</p>
+    </div>
+  </article>
+  <article class="clearer">
+    <i class="fa fa-github-alt"></i>
+    <div>
+      <h3>Open source</h3>
+      <p>Our code is free as in freedom and hosted on Github. We accept external contributions, and we do our best to integrate our own patches into upstream projects.</p>
+    </div>
+  </article>
+</section>
